1dd47986e7d3bcd3bdc0eb19adeedb2c011f13e6
k-okina <okinakahiro@gmail.com>
2016-10-22 02:15:56 +0900

    made model auto-loader

--- index.php
+++ index.php
@@ -10,3 +10,19 @@ foreach ($analysis as $value) {
         break;
     }
 }
+
+//modelをインクルードします
+if (file_exists('./Models/'.$call.'.php')) {
+    include('./Models/'.$call.'.php');
+    //callを使用してインスタンス化するクラス名を作成し、使用する
+    $className = "Models\\".$call;
+    $class = new $className();
+    $ret = $class->index($analysis);
+
+    //配列キーが設定されている配列なら展開します
+    if (!is_null($ret)) {
+        if (is_array($ret)) {
+            extract($ret);
+        }
+    }
+}
